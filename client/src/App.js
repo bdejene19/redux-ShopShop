b903c01046954b769be5bb9f0dd67a16ac016a88
Bemnet Dejene <bdejene19@gmail.com>
2022-06-24 13:13:31 -0400

    Wrapped app in redux store provider to allow global access to state - currently, cart is fully complete. Still need to do categories and products

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,8 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import Success from "./pages/Success";
 import OrderHistory from "./pages/OrderHistory";
 
@@ -38,21 +40,24 @@ const client = new ApolloClient({
 });
 
 function App() {
+  console.log(store.getState());
   return (
     <ApolloProvider client={client}>
       <Router>
         <div>
           <StoreProvider>
-            <Nav />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/success" element={<Success />} />
-              <Route path="/orderHistory" element={<OrderHistory />} />
-              <Route path="/products/:id" element={<Detail />} />
-              <Route path="*" element={<NoMatch />} />
-            </Routes>
+            <Provider store={store}>
+              <Nav />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/success" element={<Success />} />
+                <Route path="/orderHistory" element={<OrderHistory />} />
+                <Route path="/products/:id" element={<Detail />} />
+                <Route path="*" element={<NoMatch />} />
+              </Routes>
+            </Provider>
           </StoreProvider>
         </div>
       </Router>
