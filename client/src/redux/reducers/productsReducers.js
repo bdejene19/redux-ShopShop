1a3f5d94cc45412ee3188ebd76e83edf00dcc7fb
Bemnet Dejene <bdejene19@gmail.com>
2022-06-28 13:15:31 -0400

    finished writing reducers - categories and products also completed. Still needs to be tested

---
+++ client/src/redux/reducers/productsReducers.js
@@ -0,0 +1,14 @@
+import { UPDATE_PRODUCTS } from "../constants/ActionTypes";
+
+const initialProducts = [];
+export const productReducer = (state = initialProducts, action) => {
+  switch (action.type) {
+    case UPDATE_PRODUCTS: {
+      return {
+        products: [...action.payload],
+      };
+    }
+    default:
+      return state;
+  }
+};
