2e6555be94acb251d8578382b0578a1a54eef0bd
Bemnet Dejene <bdejene19@gmail.com>
2022-07-04 13:01:50 -0400

    redux store linked to productList component => however reducers still need to be tested.

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,17 +1,17 @@
-import React, { useEffect } from 'react';
-import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
-import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
-import spinner from '../../assets/spinner.gif';
-
-function ProductList() {
-  const [state, dispatch] = useStoreContext();
-
-  const { currentCategory } = state;
-
+import React, { useEffect } from "react";
+import ProductItem from "../ProductItem";
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
+import { useQuery } from "@apollo/client";
+import { QUERY_PRODUCTS } from "../../utils/queries";
+import { idbPromise } from "../../utils/helpers";
+import spinner from "../../assets/spinner.gif";
+import { connect, useDispatch } from "react-redux";
+function ProductList(props) {
+  // commented out react global store useContext - replaced by redux
+  // const [state, dispatch2] = useStoreContext();
+  // const { currentCategory } = state;
+  const dispatch = useDispatch();
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
@@ -21,10 +21,10 @@ function ProductList() {
         products: data.products,
       });
       data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
+        idbPromise("products", "put", product);
       });
     } else if (!loading) {
-      idbPromise('products', 'get').then((products) => {
+      idbPromise("products", "get").then((products) => {
         dispatch({
           type: UPDATE_PRODUCTS,
           products: products,
@@ -34,19 +34,19 @@ function ProductList() {
   }, [data, loading, dispatch]);
 
   function filterProducts() {
-    if (!currentCategory) {
-      return state.products;
+    if (!props.currentCategory) {
+      return props.products;
     }
 
-    return state.products.filter(
-      (product) => product.category._id === currentCategory
+    return props.products.filter(
+      (product) => product.category._id === props.currentCategory
     );
   }
 
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {state.products.length ? (
+      {props.products.length ? (
         <div className="flex-row">
           {filterProducts().map((product) => (
             <ProductItem
@@ -67,4 +67,13 @@ function ProductList() {
   );
 }
 
-export default ProductList;
+const mapStateToProps = (state) => {
+  let { products, currentCategory } = state;
+  return {
+    products: products,
+    currentCategory: currentCategory,
+  };
+};
+
+export default connect(mapStateToProps)(ProductList);
+// export default ProductList;
